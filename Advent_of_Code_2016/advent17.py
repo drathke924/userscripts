b299e3ac4080db38b998fa07fb3e878933df9d85
alpha0924 <alpha.zero924@gmail.com>
2016-12-16 21:31:17 -0800

    Advent 17
    Need to type faster

---
+++ Advent_of_Code_2016/advent17.py
@@ -0,0 +1,43 @@
+from collections import deque
+import hashlib
+
+data = "njfxhljp"
+frontier = deque()
+pos = (0, 0)
+vault = (3, 3)
+found = False
+frontier.append((data, pos))
+maxlen = 0
+
+def neighbors(node):
+	good = "bcdef"
+	doors = ["u", "d", "l", "r"]
+	m = hashlib.md5()
+	m.update(node.encode("utf-8"))
+	hsh = m.hexdigest()
+	unlocked = []
+	for i, c in enumerate(hsh[0:4]):
+		if c in good:
+			unlocked.append(doors[i])
+	return unlocked
+
+while len(frontier) > 0:
+	working, position = frontier.popleft()
+	if position == vault:
+		if not found:
+			print(working[8:])
+			found = True
+		if len(working[8:]) > maxlen:
+			maxlen = len(working[8:])
+		continue
+	for c in neighbors(working):
+		if c == "u" and position[1] > 0:
+			frontier.append((working + "U", (position[0], position[1] - 1)))
+		elif c == "d" and position[1] < 3:
+			frontier.append((working + "D", (position[0], position[1] + 1)))
+		elif c == "l" and position[0] > 0:
+			frontier.append((working + "L", (position[0] - 1, position[1])))
+		elif c == "r" and position[0] < 3:
+			frontier.append((working + "R", (position[0] + 1, position[1])))
+
+print(maxlen)
