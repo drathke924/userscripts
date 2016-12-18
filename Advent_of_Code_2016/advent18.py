f4c5ba6032def3a1472578e5d8ce446924a86041
alpha0924 <alpha.zero924@gmail.com>
2016-12-17 21:25:12 -0800

    Advent 18
    Better practices

--- Advent_of_Code_2016/advent18.py
+++ Advent_of_Code_2016/advent18.py
@@ -1,34 +1,41 @@
+from time import clock
+
+start_time = clock()
 data = "^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^"
 
-rows = []
+def traps(floor, n):
+	rows = [floor]
+	safe = 0
 
-safe = 0
-
-rows.append(data)
-
-for i in range(399999):
-	current = ""
-	for j in range(len(data)):
-		if j < 1:
-			if rows[i][j + 1] == "^":
-				current += "^"
+	for i in range(n - 1):
+		current = ""
+		for j in range(len(floor)):
+			if j < 1:
+				if rows[i][j + 1] == "^":
+					current += "^"
+				else:
+					current += "."
+			elif j > len(floor) - 2:
+				if rows[i][j - 1] == "^":
+					current += "^"
+				else:
+					current += "."
 			else:
-				current += "."
-		elif j > len(data) - 2:
-			if rows[i][j - 1] == "^":
-				current += "^"
-			else:
-				current += "."
-		else:
-			if rows[i][j - 1] != rows[i][j + 1]:
-				current += "^"
-			else:
-				current += "."
-	rows.append(current)
+				if rows[i][j - 1] != rows[i][j + 1]:
+					current += "^"
+				else:
+					current += "."
+		rows.append(current)
 
-for i in rows:
-	safe += i.count(".")
+	for i in rows:
+		safe += i.count(".")
 
-print(len(rows))
+	return safe
 
-print(safe)
+
+print(traps(data, 40))
+print(clock() - start_time)
+
+lap_time = clock()
+print(traps(data, 399999))
+print(clock() - lap_time)
