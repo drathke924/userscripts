5e78aed45a131674f544e713db85eba65271e9e1
alpha0924 <alpha.zero924@gmail.com>
2016-12-18 21:50:51 -0800

    Advent 19
    Fast and efficient solution for part 2

--- Advent_of_Code_2016/advent19.py
+++ Advent_of_Code_2016/advent19.py
@@ -7,16 +7,36 @@ binary.append(binary.pop(0))
 print("Part one")
 print(int("".join(binary), 2))
 
-elves = []
 
-for i in range(1, data + 1):
-	elves.append(i)
 
-size = len(elves)
-while size > 1:
-	half = math.floor(size/2)
-	elves.pop(half)
-	elves.append(elves.pop(0))
+#Slow and useless
+'''
+def josephus(data):
+	elves = []
+	for i in range(1, data + 1):
+		elves.append(i)
+
 	size = len(elves)
+	while size > 1:
+		half = math.floor(size/2)
+		elves.pop(half)
+		elves.append(elves.pop(0))
+		size = len(elves)
+	return elves[0]
+'''
 
-print(elves)
+
+largest = 1
+current = 1
+working = 1
+for i in range(1, data + 1):
+	current = i
+	if working + 2 > current:
+		largest = working
+		working = 1
+	elif working < largest:
+		working += 1
+	else:
+		working += 2
+
+print(working)
