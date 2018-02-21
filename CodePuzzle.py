5866b9062553ecce5c66ed2e8b79c2d65901cd66
alpha0924 <alpha.zero924@gmail.com>
2018-02-20 19:50:39 -0800

    Shuffle Method Added

--- CodePuzzle.py
+++ CodePuzzle.py
@@ -3,8 +3,13 @@ class Deck:
 		cards = []
 		
 	def shuffleDeck():
-		tempcards = []
-		for i in range(len(deck) / 2):
+		firstHalf = cards[:(len(cards)/2)]
+		secondHalf = cards[(len(cards)/2):]
+		cards = []
+		for i in range(len(secondHalf)):
+			if len(firstHalf) > 0:
+				cards.append(firstHalf.pop(0))
+			cards.append(secondHalf.pop(0)
 			
 
 class Card:
