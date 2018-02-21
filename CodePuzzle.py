37591f46764ea7aab1cd342f020ca152a2da3ef4
alpha0924 <alpha.zero924@gmail.com>
2018-02-20 19:40:40 -0800

    First commit

---
+++ CodePuzzle.py
@@ -0,0 +1,12 @@
+class Deck:
+	def __init__(self, cards):
+		cards = []
+		
+	def shuffleDeck():
+		tempcards = []
+		for i in range(len(deck) / 2):
+			
+
+class Card:
+	def __init__(self, color, suit):
+		
