8137fa2ec1ef63275156e6767f0c90ebcd89f54d
alpha0924 <alpha.zero924@gmail.com>
2018-03-20 09:55:04 -0700

    Day 18 WIP still

--- Advent_Of_Code_2017/day18.py
+++ Advent_Of_Code_2017/day18.py
@@ -48,5 +48,50 @@ def partone(datain):
 				continue
 		step += 1
 		
+	def genericprocess(step):
+		while True:
+			instruct = list(data[step])
+			if instruct[1] not in regset:
+				regset[instruct[1]] = 0
+			try:
+				instruct[2] = int(instruct[2])
+			except ValueError:
+				instruct[2] = regset[instruct[2]]
+			except IndexError:
+				pass
+			if instruct[0] == "set":
+				regset[instruct[1]] = instruct[2]
+			elif instruct[0] == "add":
+				regset[instruct[1]] += instruct[2]
+			elif instruct[0] == "mul":
+				regset[instruct[1]] *= instruct[2]
+			elif instruct[0] == "mod":
+				regset[instruct[1]] = regset[instruct[1]] % instruct[2]
+			elif instruct[0] == "snd":
+				try:
+					instruct[1] = int(instruct[1])
+				except ValueError:
+					instruct[1] = regset[instruct[1]]
+				return ("snd", instruct[1], regset, step)
+			elif instruct[0] == "rcv":
+				return ("rcv", instruct[1], regset, step)
+			elif instruct[0] == "jgz":
+				try:
+					instruct[1] = int(instruct[1])
+				except ValueError:
+					instruct[1] = regset[instruct[1]]
+				if instruct[1] > 0:
+					step += instruct[2]
+					continue
+			step += 1
+	
+	def processone(datain, stackone, step):
+		next = genericprocess(step)
+	
+	def processtwo(datain, stacktwo, step):
+		
+	
+	def parttwo(datain):
+		
 	
 print(partone(data))
