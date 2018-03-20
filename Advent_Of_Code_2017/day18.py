42b896e5d916ae30803c47137656f067735843ff
alpha0924 <alpha.zero924@gmail.com>
2018-03-20 14:59:39 -0700

    Day 18 WIP needs rewritten

--- Advent_Of_Code_2017/day18.py
+++ Advent_Of_Code_2017/day18.py
@@ -48,50 +48,67 @@ def partone(datain):
 				continue
 		step += 1
 		
-	def genericprocess(step):
-		while True:
-			instruct = list(data[step])
-			if instruct[1] not in regset:
-				regset[instruct[1]] = 0
+def process(regset, step, stackx, stacky):
+	count = 0
+	while True:
+		instruct = list(data[step])
+		if instruct[1] not in regset:
+			regset[instruct[1]] = 0
+		try:
+			instruct[2] = int(instruct[2])
+		except ValueError:
+			instruct[2] = regset[instruct[2]]
+		except IndexError:
+			pass
+		if instruct[0] == "set":
+			regset[instruct[1]] = instruct[2]
+		elif instruct[0] == "add":
+			regset[instruct[1]] += instruct[2]
+		elif instruct[0] == "mul":
+			regset[instruct[1]] *= instruct[2]
+		elif instruct[0] == "mod":
+			regset[instruct[1]] = regset[instruct[1]] % instruct[2]
+		elif instruct[0] == "snd":
 			try:
-				instruct[2] = int(instruct[2])
+				instruct[1] = int(instruct[1])
 			except ValueError:
-				instruct[2] = regset[instruct[2]]
-			except IndexError:
-				pass
-			if instruct[0] == "set":
-				regset[instruct[1]] = instruct[2]
-			elif instruct[0] == "add":
-				regset[instruct[1]] += instruct[2]
-			elif instruct[0] == "mul":
-				regset[instruct[1]] *= instruct[2]
-			elif instruct[0] == "mod":
-				regset[instruct[1]] = regset[instruct[1]] % instruct[2]
-			elif instruct[0] == "snd":
-				try:
-					instruct[1] = int(instruct[1])
-				except ValueError:
-					instruct[1] = regset[instruct[1]]
-				return ("snd", instruct[1], regset, step)
-			elif instruct[0] == "rcv":
-				return ("rcv", instruct[1], regset, step)
-			elif instruct[0] == "jgz":
-				try:
-					instruct[1] = int(instruct[1])
-				except ValueError:
-					instruct[1] = regset[instruct[1]]
-				if instruct[1] > 0:
-					step += instruct[2]
-					continue
-			step += 1
+				instruct[1] = regset[instruct[1]]
+			stacky.append(instruct[1])
+			count += 1
+		elif instruct[0] == "rcv":
+			if stackx != []:
+				regset[instruct[1]] = stackx.pop(-1)
+			else:
+				return (regset, step, stackx, count)
+		elif instruct[0] == "jgz":
+			try:
+				instruct[1] = int(instruct[1])
+			except ValueError:
+				instruct[1] = regset[instruct[1]]
+			if instruct[1] > 0:
+				step += instruct[2]
+				continue
+		step += 1
 	
-	def processone(datain, stackone, step):
-		next = genericprocess(step)
-	
-	def processtwo(datain, stacktwo, step):
-		
-	
-	def parttwo(datain):
-		
-	
+def parttwo(datain):
+	regone = {'p': 0}
+	regtwo = {'p': 1}
+	stackone = []
+	stacktwo = []
+	stepone = 0
+	steptwo = 0
+	sendcount = 0
+	changed = True
+	test = 0
+	while changed:
+		regone, stepone, stackone, tempcount = process(regone, stepone, stackone, stacktwo)
+		regtwo, steptwo, stacktwo, ignore = process(regtwo, steptwo, stacktwo, stackone)
+		if tempcount + ignore != test:
+			test = tempcount + ignore
+			sendcount += tempcount
+		else:
+			changed = False
+	return(sendcount)
+
 print(partone(data))
+print(parttwo(data))
