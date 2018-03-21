91309590437c3ea9f8ddb692c2cc9853f89ac44b
alpha0924 <alpha.zero924@gmail.com>
2018-03-21 13:02:49 -0700

    Getting ready to rewrite day18

--- Advent_Of_Code_2017/day18.py
+++ Advent_Of_Code_2017/day18.py
@@ -8,7 +8,7 @@ def partone(datain):
 	step = 0
 	lastsound = 0
 	while True:
-		instruct = list(data[step])
+		instruct = list(datain[step])
 		if instruct[1] not in regset:
 			regset[instruct[1]] = 0
 		try:
@@ -47,50 +47,11 @@ def partone(datain):
 				step += instruct[2]
 				continue
 		step += 1
-		
-def process(regset, step, stackx, stacky):
-	count = 0
-	while True:
-		instruct = list(data[step])
-		if instruct[1] not in regset:
-			regset[instruct[1]] = 0
-		try:
-			instruct[2] = int(instruct[2])
-		except ValueError:
-			instruct[2] = regset[instruct[2]]
-		except IndexError:
-			pass
-		if instruct[0] == "set":
-			regset[instruct[1]] = instruct[2]
-		elif instruct[0] == "add":
-			regset[instruct[1]] += instruct[2]
-		elif instruct[0] == "mul":
-			regset[instruct[1]] *= instruct[2]
-		elif instruct[0] == "mod":
-			regset[instruct[1]] = regset[instruct[1]] % instruct[2]
-		elif instruct[0] == "snd":
-			try:
-				instruct[1] = int(instruct[1])
-			except ValueError:
-				instruct[1] = regset[instruct[1]]
-			stacky.append(instruct[1])
-			count += 1
-		elif instruct[0] == "rcv":
-			if stackx != []:
-				regset[instruct[1]] = stackx.pop(-1)
-			else:
-				return (regset, step, stackx, count)
-		elif instruct[0] == "jgz":
-			try:
-				instruct[1] = int(instruct[1])
-			except ValueError:
-				instruct[1] = regset[instruct[1]]
-			if instruct[1] > 0:
-				step += instruct[2]
-				continue
-		step += 1
-	
-def parttwo(datain):
+
+def process(datain):
+	return datain
+
+def parttwo():
 	regone = {'p': 0}
 	regtwo = {'p': 1}
 	stackone = []
@@ -111,4 +72,4 @@ def parttwo(datain):
 	return(sendcount)
 
 print(partone(data))
-print(parttwo(data))
+print(parttwo())
