b32097166ffd1ae3f25876189f322e55bc834c39
alpha0924 <alpha.zero924@gmail.com>
2018-04-04 08:02:06 -0700

    Working on Day 21

--- Advent_Of_Code_2017/day21.py
+++ Advent_Of_Code_2017/day21.py
@@ -1,6 +1,39 @@
 with open("day21test.txt", "r") as f:
     DATA = f.read().splitlines()
 
+for i in range(len(DATA)):
+    DATA[i] = DATA[i].split(" ")
+
+DATADICT = {}
+
+for l in DATA:
+    DATADICT[l[0]] = l[2]
+
+
+
 start = ".#./..#/###"
 
-for i in range(2):
+def hFlip(inp):
+    out = []
+    for s in inp.split("/"):
+        out.append(s[::-1])
+    return "/".join(out)
+
+def vFlip(inp):
+    return "/".join(inp.split("/")[::-1])
+
+def check(inp):
+    global DATA
+    doH = True
+    while True:
+        if inp in DATADICT.keys():
+            return DATADICT[inp]
+        elif doH:
+            inp = hFlip(inp)
+            doH = False
+        else:
+            inp = vFlip(inp)
+            doH = True
+
+print(check(start))
+#for i in range(2):
