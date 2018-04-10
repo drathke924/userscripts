f31cf61bf5ecd149ca1c586188f1b1e3820eadec
alpha0924 <alpha.zero924@gmail.com>
2018-04-10 16:11:14 -0700

    Finished day21

--- Advent_Of_Code_2017/day21.py
+++ Advent_Of_Code_2017/day21.py
@@ -20,10 +20,9 @@ def transpose(inp):
     out = []
     inp = inp.split("/")
     for i in range(len(inp)):
-        out.append("")
-    for i in range(len(inp)):
-        for j in range(len(inp)):
-            out[j] += inp[len(inp) - 1 - i][len(inp) - 1 - j]
+        out.append(list(inp[i]))
+    out = list(map("".join, zip(*out)))
+    #print(out)
     return "/".join(out)
 
 def vFlip(inp):
@@ -35,6 +34,7 @@ def check(inp):
     count = 0
     while count < 20:
         if inp in RULEDICT.keys():
+            #print(str(inp) + " " + str(RULEDICT[inp]))
             return RULEDICT[inp]
         elif doH:
             inp = transpose(inp)
@@ -58,6 +58,7 @@ def div(inp):
                 for k in range(2):
                     smalltemp.append(temp[i + k][j:j+2])
                 out.append("/".join(smalltemp))
+        #print(out)
         return out
     elif size % 3 == 0:
         temp = inp.split("/")
@@ -65,28 +66,32 @@ def div(inp):
             for j in range(0, size, 3):
                 smalltemp = []
                 for k in range(3):
-                    smalltemp.append(temp[i + j + k][j:j+3])
+                    smalltemp.append(temp[i + k][j:j+3])
                 out.append("/".join(smalltemp))
+        #print(out)
         return out
 
 def linejoin(inp):
     out = []
     size = int(sqrt(len(inp)))
+    #print(inp)
+    temp = list(inp)
     if size > 1:
         for i in range(len(inp)):
-            inp[i] = inp[i].split("/")
-        insize = len(inp[0])
+            temp[i] = temp[i].split("/")
+        insize = len(temp[0])
         for i in range(size):
             lines = []
             for k in range(insize):
                 lines.append("")
             for j in range(insize):
                 for k in range(size):
-                    ind = k + (i*2)
-                    lines[j] = lines[j] + inp[ind][j]
+                    ind = k + i * size
+                    #print(str(temp[ind]) + " " + str(ind) + " " + str(i) + " " + str(k) + " " + str(j) + " " + str(insize))
+                    lines[j] = lines[j] + temp[ind][j]
             out.append("/".join(lines))
     else:
-        out = inp
+        out = list(inp)
     #print(out)
     return "/".join(out)
 
@@ -94,6 +99,12 @@ def linejoin(inp):
 
 output = div(start)
 for i in range(5):
+    output = div(linejoin(output))
+    for j in range(len(output)):
+        output[j] = check(output[j])
+#print(linejoin(output))
+print(linejoin(output).count("#"))
+for i in range(13):
     output = div(linejoin(output))
     for j in range(len(output)):
         output[j] = check(output[j])
