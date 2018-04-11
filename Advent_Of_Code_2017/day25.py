af9205bd16f38f03a4a42c32fac1c088099aba57
alpha0924 <alpha.zero924@gmail.com>
2018-04-11 16:51:10 -0700

    Day25 down

--- Advent_Of_Code_2017/day25.py
+++ Advent_Of_Code_2017/day25.py
@@ -1,14 +1,27 @@
-with open("day25test.txt", "r") as f:
+with open("day25.txt", "r") as f:
     DATA = list(map(lambda x: x.split("-"), f.read().splitlines()))
 
 states = {}
 for l in DATA:
     states[l[0]] = (tuple(l[1]), tuple(l[2]))
 
-print(states)
-#steps = 12481997
-steps = 6
-
+steps = 12481997
+#steps = 6
 state = "a"
-
 position = 0
+memory = {}
+for i in range(steps):
+    if position not in memory.keys():
+        memory[position] = 0
+    if memory[position]:
+        instruct = states[state][1]
+    else:
+        instruct = states[state][0]
+    memory[position] = instruct[0] == "1"
+    if instruct[1] == "r":
+        position += 1
+    elif instruct[1] == "l":
+        position -= 1
+    state = instruct[2]
+
+print(sum(memory.values()))
