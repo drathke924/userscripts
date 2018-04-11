65ed43c6a923fd990f7ed7a4a37e7d67b2dd6b31
alpha0924 <alpha.zero924@gmail.com>
2018-04-11 16:10:39 -0700

    Day 24 completed

--- Advent_Of_Code_2017/day24.py
+++ Advent_Of_Code_2017/day24.py
@@ -1,10 +1,33 @@
-with open("day24test.txt", "r") as f:
+from operator import itemgetter
+
+with open("day24.txt", "r") as f:
     DATA = list(map(lambda x: tuple(map(int, x.split("/"))), f.read().splitlines()))
 
-print(DATA)
+nodes = {}
+for node in DATA:
+    if node[0] not in nodes.keys():
+        nodes[node[0]] = set()
+    nodes[node[0]].add(node[1])
+    if node[1] not in nodes.keys():
+        nodes[node[1]] = set()
+    nodes[node[1]].add(node[0])
 
-def does_connect(node_one, node_two, port):
-    if node_one[port] in node_two:
-        return True
+start = (0, 0)
+all_bridges = set()
 
-def make_bridge(search_set):
+def bridge_generator(current, bridge, node_set):
+    for i in node_set[current[1]]:
+        if (current[1], i) not in bridge and (i, current[1]) not in bridge:
+            yield set(list(bridge) + [(current[1], i)])
+            yield from bridge_generator((current[1], i), set(list(bridge) + [(current[1], i)]), node_set)
+
+sums = []
+lengths = []
+
+for bridge in bridge_generator(start, set(), nodes):
+    strength = sum(list(map(sum, bridge)))
+    lengths.append(len(bridge))
+    sums.append(strength)
+
+print(max(sums))
+print(sorted(list(zip(lengths, sums)), key=itemgetter(0, 1))[-1])
