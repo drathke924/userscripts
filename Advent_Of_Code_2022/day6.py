3cf333350a92fbd07678ad2504025d14b933a624
alpha0924 <alpha.zero924@gmail.com>
2022-12-06 19:25:25 -0800

    2022 day 6

---
+++ Advent_Of_Code_2022/day6.py
@@ -0,0 +1,17 @@
+import queue
+
+def find_marker(data_in, size):
+    buffer = list(data_in[0:size])
+    buffer.reverse()
+    for i in range(size, len(data_in)):
+        if len(set(buffer)) == size:
+            return i
+        else:
+            buffer.pop()
+            buffer.insert(0, data_in[i])
+
+with open("day6.txt" , "r") as f:
+    data = f.read()
+
+print("Part One: " + str(find_marker(data, 4)))
+print("Part Two: " + str(find_marker(data, 14)))
